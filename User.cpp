e09222612d8b402eb5751e56cf290d3282ac4fb2
Dutu Luiza-Alexandra <luizadutu171@gmail.com>
2026-05-21 18:46:27 +0300

    Class User
    - Implemented "User" class to represent system users
    - Added a mock password hashing mechanism "simulateHash"
    - Generated a unique dynamic "userKey" derived from credentials
    - Provided "validatePassword()" method for login verification

---
+++ User.cpp
@@ -0,0 +1,28 @@
+#include "User.h"
+#include <algorithm>
+
+User::User(const std::string& username, const std::string& password)
+    : username(username){
+
+        this->passwordHash = simulateHash(password);
+
+    this->userKey = passwordHash + "_" + username;
+    }
+
+std::string User::simulateHash( const std::string& password ) const {
+    std::string hashed = password;
+    std::reverse(hashed.begin(), hashed.end());
+    return hashed+ "secure";
+}
+
+std::string User::getUsername() const {
+    return username;
+}
+
+std::string User::getUserKey() const {
+    return userKey;
+}
+
+bool User::validatePassword(const std::string& password) const {
+    return simulateHash(password) == passwordHash;
+}
---
+++ User.h
@@ -0,0 +1,32 @@
+//
+// Created by Luiza on 21.05.2026.
+//
+
+#ifndef ENCRYPTED_VIRTUAL_FILE_SYSTEM_USER_H
+#define ENCRYPTED_VIRTUAL_FILE_SYSTEM_USER_H
+
+#include <string>
+
+class User {
+
+private:
+    std::string username;
+    std::string passwordHash;
+    std::string userKey;
+
+    std::string simulateHash(const std::string& password) const;
+
+public:
+    User(const std::string& username, const std::string& password);
+
+    std::string getUsername() const;
+    std::string getUserKey() const;
+
+    bool validatePassword( const std::string& password) const;
+
+};
+
+
+
+
+#endif //ENCRYPTED_VIRTUAL_FILE_SYSTEM_USER_H
